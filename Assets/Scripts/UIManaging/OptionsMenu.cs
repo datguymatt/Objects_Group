ae970add8e0b59a854ebe21fa52d1aa5240896f2
datguymatt <128839182+datguymatt@users.noreply.github.com>
2023-09-28 20:01:39 -0700

    added a volume fade feature for music
    tested a volume fade as the music was fading out too swiftly

--- Assets/Scripts/UIManaging/OptionsMenu.cs
+++ Assets/Scripts/UIManaging/OptionsMenu.cs
@@ -8,6 +8,8 @@ public class OptionsMenu : MonoBehaviour
     // menu gameobject assigned
     public GameObject MainMenuUI, OptionsMenuUI;
 
+    public int fadeVolumeRate;
+
     // this is where music audiomixer is assigned 
     public AudioMixer mainmixer;
     
@@ -26,10 +28,19 @@ public class OptionsMenu : MonoBehaviour
     // This is where Music is operated using Slider where -80 lowest and 0 is the highest value
     public void MusicVolumeSlider(float volume)
     {
-        // This operates the exact mixer you choice 
-        mainmixer.SetFloat("volume1", volume);
-        mainmixer.SetFloat("volume2", volume);
-        mainmixer.SetFloat("volume3", volume);
+        // This operates the exact mixer you choice
+        //fade out made more smooth by the fadeVolumeRate
+        if (fadeVolumeRate != 0)
+        {
+            mainmixer.SetFloat("volume1", (volume / fadeVolumeRate));
+            if(volume < -75)
+            {
+                mainmixer.SetFloat("volume1", -80);
+            }
+        }
+        
+        //mainmixer.SetFloat("volume2", volume);
+        //mainmixer.SetFloat("volume3", volume);
     }
     // This is where Sound is operated using Slider where -80 lowest and 0 is the highest value
     //public void SoundVolumeSlider(float volume)
